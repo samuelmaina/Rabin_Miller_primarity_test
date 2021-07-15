248775013cb8dc57235de9356c6808f96d6a7108
samuelmayna <samuelmayna@gmail.com>
2021-07-15 10:46:48 +0300

    Added Rabin Miller algorithm file

---
+++ src/com/company/MillerRabin.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.math.BigInteger;
+
+public class MillerRabin {
+    private BigInteger k;
+    private BigInteger m;
+    private BigInteger prime;
+
+    public void setK(BigInteger k) {
+        this.k = k;
+    }
+
+    public void setM(BigInteger m) {
+        this.m = m;
+    }
+
+    public BigInteger getK() {
+        return k;
+    }
+
+    public BigInteger getM() {
+        return m;
+    }
+
+    public MillerRabin(BigInteger prime) {
+        this.prime = prime;
+    }
+
+    public void findKandM() {
+        BigInteger two = BigInteger.TWO;
+        BigInteger one= BigInteger.ONE;
+        BigInteger s = prime.subtract(BigInteger.ONE);
+        while (s.mod(two).equals(BigInteger.ZERO)) {
+            s = s.divide(two);
+            k.add(one);
+        }
+        m=s;
+    }
+}
